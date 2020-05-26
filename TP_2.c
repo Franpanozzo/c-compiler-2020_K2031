a4849466b2cf98ad86d147d5931860e5dfaccec7
octavioramirez12 <octavio.ramirez.3980@gmail.com>
2020-05-25 22:53:29 -0300

    Se agrega ciclo para evaluar varias exp. Se detectan caracteres invalidos

--- TP_2.c
+++ TP_2.c
@@ -27,60 +27,76 @@ char pop (nodo** pila);
 
 int main()
 {
-    char exp [20];
-    printf("Ingrese la expresion a evaluar: ");
-    scanf("%s",exp);
 
-    est trans [6][5] = {
-        {{3, "$"},{1, "$"}, {3, "$"}, {0, "R$"}, {3, "$"}},  // q0 $
-        {{1, "$"}, {1, "$"}, {0, "$"}, {3, "$"}, {3, "$"}},  // q1 $
-        {{3, "R"}, {1, "R"}, {3, "$"}, {0, "RR"}, {3, "R"}}, // q0 R
-        {{1, "R"}, {1, "R"}, {0, "R"}, {3, "R"}, {2, ""}},   // q1 R
-        {{3, "R"}, {3, "R"}, {0, "R"}, {3, "R"}, {2, ""}},   // q2 R
-        {{3, "$"}, {3, "$"}, {0, "$"}, {3, "$"}, {3, "$"}},  // q2 $
+    est trans [6][6] = {
+        {{3, "$"},{1, "$"}, {3, "$"}, {0, "R$"}, {3, "$"}, {3, "$"}},  // q0 $
+        {{1, "$"}, {1, "$"}, {0, "$"}, {3, "$"}, {3, "$"}, {3, "$"}},  // q1 $
+        {{3, "R"}, {1, "R"}, {3, "$"}, {0, "RR"}, {3, "R"}, {3, "R"}}, // q0 R
+        {{1, "R"}, {1, "R"}, {0, "R"}, {3, "R"}, {2, ""}, {3, "R"}},   // q1 R
+        {{3, "R"}, {3, "R"}, {0, "R"}, {3, "R"}, {2, ""}, {3, "R"}},   // q2 R
+        {{3, "$"}, {3, "$"}, {0, "$"}, {3, "$"}, {3, "$"}, {3, "$"}},  // q2 $
         };
 
-
+    char exp [20];
     int columna;
     int fila;
-    int estadoActual = 0;
+    int estadoActual;
     est estadoDeTabla;
-    int i = 0, j=0;
-    char caracterActual = exp[i];
-    nodo* pila;
-    push (&pila, '$');
+    int i , j;
+    char caracterActual;
+    nodo* pila = NULL;
+
     char cimaDePila;
-    while (caracterActual != '\0')
+    char charControl = 's';
+
+    while (charControl != 'n' && charControl != 'N')
     {
+        i = j = estadoActual = 0;
+        vaciarPila (&pila);
+        push (&pila, '$');
+
+        system("cls");
+        printf("Ingrese la expresion a evaluar: ");
+        scanf("%s",exp);
+        caracterActual = exp[i];
+
+        while (caracterActual != '\0' && estadoActual != 3)
+        {
+            cimaDePila = pop(&pila);
+            columna = determinarColumna(caracterActual);
+            fila = determinarFila(estadoActual , cimaDePila);
+
+            estadoDeTabla = trans[fila][columna];
+            estadoActual = estadoDeTabla.estadoSiguiente;
+
+            if (estadoDeTabla.estadoSiguiente == 3)
+            {
+                printf("Hubo un error de sintaxis en la posicion %d\n", j);
+            }
+
+            for(int h = strlen(estadoDeTabla.cadenaPush) - 1; h >= 0 ; h--)
+            {
+                push(&pila, estadoDeTabla.cadenaPush[h]);
+            }
+
+            j++;
+            caracterActual = exp[++i];
+        }
+
         cimaDePila = pop(&pila);
-        columna = determinarColumna(caracterActual);
-        fila = determinarFila(estadoActual , cimaDePila);
-        estadoDeTabla = trans[fila][columna];
-        estadoActual = estadoDeTabla.estadoSiguiente;
-        if (estadoDeTabla.estadoSiguiente == 3)
+
+        if(esEstadoFinal(estadoActual, cimaDePila))
         {
-            printf("Hubo un error de sintaxis en la posicion %d\n", j);
-            break;
+            printf("La expresion es sintacticamente correcta :D\n");
+        }
+        else
+        {
+            printf("La expresion no es sintacticamente correcta D:\n");
         }
 
-        for(int h = strlen(estadoDeTabla.cadenaPush) - 1; h >= 0 ; h--)
-        {
-            push(&pila, estadoDeTabla.cadenaPush[h]);
-        }
-
-        j++;
-        caracterActual = exp[++i];
-    }
-
-    cimaDePila = pop(&pila);
-
-    if(esEstadoFinal(estadoActual, cimaDePila))
-    {
-        printf("La expresion es sintacticamente correcta :D\n", j);
-    }
-    else
-    {
-        printf("La expresion no es sintacticamente correcta");
+        printf("Quiere ingresar otra expresion? (s/n): ");
+        fflush(stdin);
+        charControl = getc(stdin);
     }
 
     return 0;
@@ -91,13 +107,21 @@ int esEstadoFinal(int estado, char cimaDePila)
     return (estado == 1 || estado == 2) && cimaDePila == '$';
 }
 
+void vaciarPila(nodo ** pila)
+{
+    while (*pila != NULL)
+    {
+        pop(pila);
+    }
+}
+
 int determinarColumna(char caracter){
     if(caracter=='0') return 0;
     else if('1' <= caracter && caracter <= '9') return 1;
     else if(caracter == '+' || caracter =='-' || caracter == '*' || caracter == '/') return 2;
     else if ( caracter == '(') return 3;
     else if(caracter == ')') return 4;
-    else return -1;
+    else return 5;
 }
 
 void push (nodo** pila, char x)
